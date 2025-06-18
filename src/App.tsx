83a744120823ced7228e6e5f1aeb418306e1f0e8
SchubertChris <schubert_chris@rocketmail.com>
2025-06-18 03:03:25 +0200

    Styling Base

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import './Assets/Style/Base/_Index.scss'
+import '@s/_Index.scss'
 
+// Beispiele für die neuen Alias-Imports:
+// import SomeComponent from '@c/SomeComponent'
+// import Layout from '@l/Layout'
+// import HomePage from '@p/HomePage'
+// import LoginForm from '@f/Auth/LoginForm'
+// import { someUtility } from '@u/helpers'
+// import { SomeType } from '@ty/types'
 
 function App() {
-
   return (
     <>
-
+      <h1>React Shop System</h1>
+      <p>Welcome to the React Shop System!</p>
+      {/* Hier können weitere Komponenten und Routen hinzugefügt werden */}
     </>
   )
 }
